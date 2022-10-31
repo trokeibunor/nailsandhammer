88b073ff035c8410b2744a6f5b0d184e3cbcb267
Okeibunor Emmanuel <okeibunoremma@gmail.com>
2022-10-31 21:22:43 +0100

    finished home page rough draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BestSellerComponent } from './components/best-seller/best-seller.component';
+import { NewsletterComponent } from './components/newsletter/newsletter.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BestSellerComponent } from './components/best-seller/best-seller.compon
     HomeComponent,
     NavbarComponent,
     FooterComponent,
-    BestSellerComponent
+    BestSellerComponent,
+    NewsletterComponent
   ],
   imports: [
     BrowserModule,
